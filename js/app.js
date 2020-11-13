6cb5749fd947f88cd7603d4bca33eab9ee9b598a
Thanikul Sriuthis <moremeng@dv4.biz>
2020-11-13 17:24:55 +0700

    SIMPLE SLIDESHOW

---
+++ js/app.js
@@ -0,0 +1,1 @@
+window.addEventListener("DOMContentLoaded", (e) => { let stage = document.getElementById("stage"); let slideComplete = (e) => { stage.appendChild(arr[0]); }; let arr = stage.getElementsByTagName("p"); for (let i = 0; i < arr.length; i++) { arr[i].addEventListener("animationend", slideComplete, false); } }, false);
